c9feea206ac7dd40ff2e259f1e550045a4e4a84d
ilya034 <68101303+ilya034@users.noreply.github.com>
2025-12-03 22:59:18 +0500

    UIManager.cs + Settings Button

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject mainMenuPanel;
+    public GameObject gamePanel;
+    public GameObject savesPanel;
+    public GameObject settingsPanel;
+    public GameObject deskUpgradesPanel;
+    public GameObject roomUpgradesPanel;
+
+    private void HideAll()
+    {
+        mainMenuPanel.SetActive(false);
+        gamePanel.SetActive(false);
+        savesPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        deskUpgradesPanel.SetActive(false);
+        roomUpgradesPanel.SetActive(false);
+    }
+
+    public void ShowMainMenu()
+    {
+        HideAll();
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void ShowGame()
+    {
+        HideAll();
+        gamePanel.SetActive(true);
+    }
+
+    public void ShowSettings()
+    {
+        HideAll();
+        settingsPanel.SetActive(true);
+    }
+
+    public void ShowSaves()
+    {
+        HideAll();
+        savesPanel.SetActive(true);
+    }
+}
