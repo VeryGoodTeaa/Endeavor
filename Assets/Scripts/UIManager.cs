beccbabf74d117f730b5829c50e79110d43666f0
ilya034 <68101303+ilya034@users.noreply.github.com>
2025-12-03 23:22:42 +0500

    UIManager rework + back buttons

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,50 +1,66 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Панели")]
     public GameObject mainMenuPanel;
     public GameObject gamePanel;
-    public GameObject savesPanel;
     public GameObject settingsPanel;
+    public GameObject savesPanel;
     public GameObject deskUpgradesPanel;
     public GameObject roomUpgradesPanel;
 
-    private void Start()
+    private GameObject currentPanel;
+    private Stack<GameObject> openedPanels = new();
+
+    void Start()
     {
-        ShowMainMenu();
+        CloseAllPanels();
+        currentPanel = mainMenuPanel;
+        currentPanel.SetActive(true);
     }
 
-    private void HideAll()
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (openedPanels.Count > 0)
+                GoBack();
+        }
+    }
+
+    public void OpenPanel(GameObject newPanel)
+    {
+        if (currentPanel != null)
+        {
+            openedPanels.Push(currentPanel);
+            currentPanel.SetActive(false);
+        }
+
+        newPanel.SetActive(true);
+        currentPanel = newPanel;
+    }
+
+    public void GoBack()
+    {
+        if (openedPanels.Count <= 0)
+            return;
+
+        currentPanel.SetActive(false);
+        GameObject previousPanel = openedPanels.Pop();
+
+        previousPanel.SetActive(true);
+        currentPanel = previousPanel;
+    }
+
+    private void CloseAllPanels()
     {
         mainMenuPanel.SetActive(false);
         gamePanel.SetActive(false);
-        savesPanel.SetActive(false);
         settingsPanel.SetActive(false);
+        savesPanel.SetActive(false);
         deskUpgradesPanel.SetActive(false);
         roomUpgradesPanel.SetActive(false);
-    }
-
-    public void ShowMainMenu()
-    {
-        HideAll();
-        mainMenuPanel.SetActive(true);
-    }
-
-    public void ShowGame()
-    {
-        HideAll();
-        gamePanel.SetActive(true);
-    }
-
-    public void ShowSettings()
-    {
-        HideAll();
-        settingsPanel.SetActive(true);
-    }
-
-    public void ShowSaves()
-    {
-        HideAll();
-        savesPanel.SetActive(true);
     }
 }
